fe1a5011e8f2cd63443f6f7c419abc394b87876b
Kieberts <65255153+Kieberts@users.noreply.github.com>
2021-06-30 23:55:28 +0200

    complete Form

--- indexAusleihen.php
+++ indexAusleihen.php
@@ -7,22 +7,29 @@
     <script src="./js/jquery/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="./css/stylesDetail.css">
     <script src="./js/DetailView.js" charset="utf-8"></script>
+
   </head>
   <body>
     <h1 id="headerName"></h1>
 
-    <form id="formID">
+    <form id="formID" method="get" action="indexAusgeliehen.php">
+        <div>
+          <input id="idTag" name="item" type="hidden" value="<?php echo $_GET['item']; ?>">
+        </div>
+        <div>
+          <input id="geraetTag" name="name" type="hidden" value="<?php echo $_GET['name']; ?>">
+        </div>
         <div>
           <label>Name:</label>
-          <input id="name" name="name" type="text">
+          <input name="nameAusleiher" type="text">
         </div>
         <div>
           <label>Datum:</label>
-          <input id="datum" name="date" type="date">
+          <input name="dateAusleiher" type="date">
         </div>
         <div>
-          <input id="ausleihen" type="submit" name="update" value="Ausleihen"> <!-- Sends status to MYSQL DATABASE -->
-          <input id="abbrechen" type="submit" name="cancel" value="Abbrechen"> <!-- Returns to index.php -->
+          <input type="submit" value="Ausleihen"> <!-- Sends status to MYSQL DATABASE -->
+          <button type="button" name="button" onclick="window.location.href='index.php'">Abbrechen</button>
         </div>
      </form>
   </body>
