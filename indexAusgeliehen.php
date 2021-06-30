72e2c0170d5e9dbdfc2b5f3096b5299194b411e1
Kieberts <65255153+Kieberts@users.noreply.github.com>
2021-06-30 23:44:18 +0200

    Add indexAusgeliehen.php

---
+++ indexAusgeliehen.php
@@ -0,0 +1,22 @@
+<?php include("./php/database.php");
+  //echo updateAbgeben($_GET['item'],$_GET['name']);
+?>
+
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Abgabe</title>
+    <style type="text/css">
+      body {
+        width: 700px;
+        margin: auto;
+        font-size: 20px;
+      }
+    </style>
+  </head>
+  <body>
+    <p>Erfolgreich ausgeliehen</p>
+    <p><a href="index.php">Zurück</a></p>
+  </body>
+</html>
