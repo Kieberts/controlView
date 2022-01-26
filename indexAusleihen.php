f130fba875a87807c8a46ad47e3537d9edd7fc08
Kieberts <65255153+Kieberts@users.noreply.github.com>
2022-01-26 20:40:01 +0100

    update Ausleih View

--- indexAusleihen.php
+++ indexAusleihen.php
@@ -2,7 +2,7 @@
 <html lang="de" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Test</title>
+    <title>Ausleih Formular</title>
 
     <script src="./js/jquery/jquery-3.6.0.min.js"></script>
     <link rel="stylesheet" href="./css/stylesDetail.css">
@@ -10,27 +10,38 @@
 
   </head>
   <body>
-    <h1 id="headerName"></h1>
+    <div id="content">
+      <div>
+        <h1>Ausleihformular</h1>
+      </div>
+      <div >
+        <p id="headerName">Gerätname: </p>
+        <p class="hint">Hier könnten noch mehr Informationen wie ID des Geräts aufgelisten werden</p>
+        <form method="post" action="indexAusgeliehen.php">
+            <div class="formID-input">
+              <input id="idTag" name="item" type="hidden" value="<?php echo $_GET['item']; ?>">
+            </div>
+            <div class="formID-input">
+              <input id="geraetTag" name="name" type="hidden" value="<?php echo $_GET['name']; ?>">
+            </div>
+            <div class="formID-input">
+              <label id="pflichtFeldName">Name*:</label>
+              <input id="inputName" name="nameAusleiher" type="text" onchange="checkInput()" placeholder="Max Mustermann">
+            </div>
+            <div class="formID-input">
+              <label id="pflichtFeldDatum">Datum*:</label>
+              <input id="inputDate" name="dateAusleiher" type="date" onchange="checkInput()">
+            </div>
+            <div class="formID-input">
+              <input id="send" type="submit" value="Ausleihen"> <!-- Sends status to MYSQL DATABASE -->
+              <button type="button" name="button" onclick="window.location.href='index.php'">Abbrechen</button>
+            </div>
+         </form>
+         <div>
+           <p id="pflicht">*Pflichtfeld</p>
+         </div>
+      </div>
+    </div>
 
-    <form id="formID" method="post" action="indexAusgeliehen.php">
-        <div>
-          <input id="idTag" name="item" type="hidden" value="<?php echo $_GET['item']; ?>">
-        </div>
-        <div>
-          <input id="geraetTag" name="name" type="hidden" value="<?php echo $_GET['name']; ?>">
-        </div>
-        <div>
-          <label>Name:</label>
-          <input name="nameAusleiher" type="text">
-        </div>
-        <div>
-          <label>Datum:</label>
-          <input name="dateAusleiher" type="date">
-        </div>
-        <div>
-          <input type="submit" value="Ausleihen"> <!-- Sends status to MYSQL DATABASE -->
-          <button type="button" name="button" onclick="window.location.href='index.php'">Abbrechen</button>
-        </div>
-     </form>
   </body>
 </html>
